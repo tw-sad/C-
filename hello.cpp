5e001c19e88713eebafbed736f6fa80640db70f1
tw-sad <62336215+tw-sad@users.noreply.github.com>
2020-07-04 12:40:37 +0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+# C-
+/ *
+c語言 函數printf():依據特定格式將文字訊息列印到螢幕上
+#include <stdio.h>
+int main(){
+	printf（“ hello world！\ n”）； //將hello world文字訊息列印到螢幕上 \n換行字元
+	返回0;
+}
+*/
+#include <iostream>
+int main(){
+	std::cout<<"hello world"; //std命名空間 (:: scope意思是"範圍"、"屬於")cout代表螢幕的物件(輸出)
+	return 0;
+}
